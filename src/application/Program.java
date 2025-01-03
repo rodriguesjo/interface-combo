2866f4576faf86796fd9cb4a249899564fbc6aa7
Jo Boettger <rodriguesjo8@outlook.com>
2025-01-02 22:08:58 -0300

    Versão final com interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,29 @@ package application;
 
 
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Printer p = new Printer("1080");
+		ConcretePrinter p = new ConcretePrinter("1080");
 		p.processDoc("my letter");
 		p.print("My letter");
 		
-		Scanner s = new Scanner("2003");
+		System.out.println("-----------------");
+		ConcreteScanner s = new ConcreteScanner("2003");
 		s.processDoc("My Email");
 		System.out.println("Scan result: " + s.scan());
+		
+		System.out.println("-----------------");
+		ComboDevice cd = new ComboDevice("2081");
+		cd.processDoc("My dissertation");
+		cd.processDoc("My dissertation");
+		System.out.println("Scan result: " + cd.scan());
 	}
+	
 
 }
